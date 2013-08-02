4aeb8f309853b852dc04273d531f6a4dcb65f04d
Aparna Chaudhary <aparna.chaudhary@asml.com>
2013-08-02 16:15:06 +0200

    Added for command line demo.

---
+++ src/main/java/net/arunoday/liquibase/demo/Main.java
@@ -0,0 +1,15 @@
+package net.arunoday.liquibase.demo;
+
+/**
+ * Tests Liquibase command line options
+ * 
+ * @author achaudha
+ * 
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		//TODO: Implementation
+	}
+
+}
